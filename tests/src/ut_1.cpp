5bd6359f0728e6e367a3694201cd838d2dea2965
Sebanisu <peeceseven@gmail.com>
2021-03-01 11:33:23 -0500

    setting up test

--- tests/src/ut_1.cpp
+++ tests/src/ut_1.cpp
@@ -1,3 +1,15 @@
 #include <boost/ut.hpp>// single header
 // import boost.ut;        // single module (C++20)
-int main() {}
+#include "tl/read.hpp"
+int main()
+{
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+  using namespace boost::ut;
+
+  int umm {};
+  "hello world"_test = [&umm] {
+    int i = umm;
+    expect(42_i == i);
+  };
+}
