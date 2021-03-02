aa1aeda41b8c74165b8fd958bbd5bb58f61da109
Sebanisu <peeceseven@gmail.com>
2021-03-01 20:06:10 -0500

    more read overloads

--- src/tl/read.hpp
+++ src/tl/read.hpp
@@ -59,6 +59,49 @@ requires(sizeof...(outputT) > 1U) void value(std::span<const char> input,
 {
   value(&input, output...);
 }
+/**
+ *
+ * @tparam outputT trivially_copyable, need to take sizeof(outputT)
+ * @param input stream of data.
+ * @param output where the input is wrote to.
+ */
+template<concepts::is_trivially_copyable... outputT>
+void value(std::istream &input, outputT &...output)
+{
+  constexpr auto         size = (sizeof(outputT) + ...);
+  std::array<char, size> tmp{};
+  input.read(std::ranges::data(tmp), size);
+  value(tmp, output...);
+}
+template<concepts::is_trivially_copyable_and_default_constructible outputT>
+outputT value(std::span<const char> input)
+{
+  auto output = outputT{};
+  value(input,output);
+  return output;
+}
+
+template<concepts::is_trivially_copyable_and_default_constructible outputT>
+outputT value(std::span<const char> * const input)
+{
+
+  auto output = value<outputT>(*input);
+  *input = input->subspan(sizeof(outputT));
+  return output;
+}
+
+template<concepts::is_trivially_copyable_and_default_constructible... outputT>
+requires(sizeof...(outputT)>1)
+auto value(std::span<const char> * const input)
+{
+  return std::tuple<outputT...>{value<outputT>(input)...};
+}
+template<concepts::is_trivially_copyable_and_default_constructible... outputT>
+requires(sizeof...(outputT)>1)
+auto value(std::span<const char> input)
+{
+  return value<outputT...>(&input);
+}
 namespace safe {
   /**
    * Safe Read Value it checks the size of the span before hand.
@@ -105,6 +148,35 @@ namespace safe {
     } else {
       (value(&input, output), ...);
     }
+  }
+  void sizeof_stream(std::istream &              input,
+                     const std::fpos<mbstate_t> &current,
+                     const std::fpos<mbstate_t> &end);
+  auto sizeof_stream(std::istream &input)
+  {
+    const auto current = input.tellg();
+    input.seekg(0, std::ios::end);
+    const auto end = input.tellg();
+    input.seekg(current, std::ios::beg);
+    return static_cast<std::size_t>(end - current);
+  }
+  /**
+   * Read all these values from stream
+   * @tparam outputT trivially_copyable, need to take sizeof(outputT)
+   * @param input stream of data.
+   * @param output where the input is wrote to.
+   */
+  template<concepts::is_trivially_copyable... outputT>
+  void value(std::istream &input, outputT &...output)
+  {
+    auto       remaining = sizeof_stream(input);
+    const auto lambda    = [&input, &remaining](auto &one_item) {
+      if (remaining >= sizeof(one_item)) {
+        read::value(input, one_item);
+        remaining -= sizeof(one_item);
+      }
+    };
+    (lambda(output), ...);
   }
 }// namespace safe
 }// namespace tl::read
