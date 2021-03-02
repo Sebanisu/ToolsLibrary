71b625a90cdf1e444fa1e855681d8b9c7c4d3d5d
Sebanisu <peeceseven@gmail.com>
2021-03-02 12:40:40 -0500

    new concept

--- src/tl/concepts.hpp
+++ src/tl/concepts.hpp
@@ -5,6 +5,14 @@ namespace tl::concepts {
 template<typename T>
 concept is_trivially_copyable = std::is_trivially_copyable_v<T>;
 template<typename T>
-concept is_trivially_copyable_and_default_constructible = is_trivially_copyable<T> && std::is_default_constructible_v<T>;
-}
+concept is_trivially_copyable_and_default_constructible =
+  is_trivially_copyable<T> &&std::is_default_constructible_v<T>;
+template<typename T> concept is_resizable = requires(T t)
+{
+  t.resize(1U);
+};
+template<typename T>
+concept is_contiguous_and_resizable =
+  std::ranges::contiguous_range<T> &&is_resizable<T>;
+}// namespace tl::concepts
 #endif// TOOLSLIBRARY_CONCEPTS_HPP
