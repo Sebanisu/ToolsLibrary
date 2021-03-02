7bd5e585297d63da6d5a3e5ef1c5633b4cfa127b
Sebanisu <peeceseven@gmail.com>
2021-03-01 20:05:44 -0500

    added is_trivially_copyable_and_default_constructible

--- src/tl/concepts.hpp
+++ src/tl/concepts.hpp
@@ -4,5 +4,7 @@
 namespace tl::concepts {
 template<typename T>
 concept is_trivially_copyable = std::is_trivially_copyable_v<T>;
+template<typename T>
+concept is_trivially_copyable_and_default_constructible = is_trivially_copyable<T> && std::is_default_constructible_v<T>;
 }
 #endif// TOOLSLIBRARY_CONCEPTS_HPP
