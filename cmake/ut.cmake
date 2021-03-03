2c53f4cfbd865702e5ebd03f130be1e4025dcf86
Sebanisu <peeceseven@gmail.com>
2021-03-03 18:46:02 -0500

    new version of ut.cmake
    switched to master branch instead of using v1.1.8

--- cmake/ut.cmake
+++ cmake/ut.cmake
@@ -3,6 +3,11 @@ include(FetchContent)
 FetchContent_Declare(
         ut
         GIT_REPOSITORY https://github.com/boost-ext/ut.git
-        GIT_TAG        v1.1.8
+        GIT_TAG        origin/master
 )
-FetchContent_MakeAvailable(ut)
+#FetchContent_MakeAvailable(ut)
+FetchContent_GetProperties(ut)
+if(NOT ut_POPULATED)
+    FetchContent_Populate(ut)
+    add_subdirectory(${ut_SOURCE_DIR} ${ut_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
