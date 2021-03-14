0e29ddbdc031eac9e141af7aeacad23a6232f9ce
Sebanisu <peeceseven@gmail.com>
2021-03-14 14:03:17 -0400

    added drive letter removal function and tests.

--- src/tl/string.hpp
+++ src/tl/string.hpp
@@ -1,7 +1,36 @@
-//
-// Created by pcvii on 3/14/2021.
-//
 #ifndef TOOLSLIBRARY_STRING_HPP
 #define TOOLSLIBRARY_STRING_HPP
+#include <ranges>
+#include <algorithm>
+namespace tl::string
+{
 
+/**
+ * Remove c:\ drive letter from start of file.
+ * @param input path string
+ */
+static void
+remove_drive_letter(std::string &input)
+{
+  constexpr static auto letters =
+    std::string_view("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  if (std::ranges::size(input) >=3 && input[1] == ':' && (input[2] == '\\' || input[2] == '/')
+      && std::ranges::any_of(letters, [&input](const char &c) {
+    return input[0] == c;
+  })) {
+    input.erase(0, 3);// remove c:\ from the start of the strings.
+  }
+}
+/**
+ * Remove c:\ drive letter from start of file.
+ * @param input path string
+ * @return modified string
+ */
+static std::string
+remove_drive_letter(std::string &&input)
+{
+  remove_drive_letter(input);
+  return std::move(input);
+}
+}
 #endif// TOOLSLIBRARY_STRING_HPP
--- tests/src/sequence_test.cpp
+++ tests/src/sequence_test.cpp
@@ -3,33 +3,20 @@
 #include "tl/utility.hpp"
 #include <string_view>
 int
-main()
+  main()
 {
   using namespace boost::ut::literals;
   using namespace boost::ut::operators::terse;
   using namespace boost::ut;
   using namespace std::string_view_literals;
   using namespace std::string_literals;
-  [[maybe_unused]] suite random = [] {
-    const auto check_type = []<std::integral integralT>() {
-      const auto random_values = tl::random::iota<char, 10U>();
-      expect(std::ranges::any_of(random_values, [](const auto &value) -> bool {
-        return value != integralT{};
-      }));
-    };
-    const auto check_types = [&check_type]<std::integral... integralT>()
-    {
-      (check_type.template operator()<integralT>(), ...);
-    };
-    "create random char array"_test = [&check_types] {
-      check_types.operator()<std::int8_t,
-        std::uint8_t,
-        std::int16_t,
-        std::uint16_t,
-        std::int32_t,
-        std::uint32_t,
-        std::int64_t,
-        std::uint64_t>();
+  [[maybe_unused]] suite sequence = [] {
+    "create random char array"_test = [] {
+      auto s = tl::utility::sequence<0, 10>();
+      s([]<auto I>() {
+        expect(I <= 10_ull);
+        expect(I >= 0_ull);
+      });
     };
   };
 }
