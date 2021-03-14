91f4c04667f05f5dec1275ab194a108d9febe197
Sebanisu <peeceseven@gmail.com>
2021-03-14 10:28:20 -0400

    better names in test

--- tests/src/random_test.cpp
+++ tests/src/random_test.cpp
@@ -11,15 +11,15 @@ int
   using namespace std::string_view_literals;
   using namespace std::string_literals;
   [[maybe_unused]] suite random = [] {
-    const auto check_type = []<std::integral T>() {
-      const auto i = tl::random::iota<char, 10U>();
-      expect(std::ranges::any_of(i, [](const auto &value) -> bool {
-        return value != T{};
+    const auto check_type = []<std::integral integralT>() {
+      const auto random_values = tl::random::iota<char, 10U>();
+      expect(std::ranges::any_of(random_values, [](const auto &value) -> bool {
+        return value != integralT{};
       }));
     };
-    const auto check_types = [&check_type]<std::integral... T>()
+    const auto check_types = [&check_type]<std::integral... integralT>()
     {
-      (check_type.template operator()<T>(), ...);
+      (check_type.template operator()<integralT>(), ...);
     };
     "create random char array"_test = [&check_types] {
       check_types.operator()<std::int8_t,
