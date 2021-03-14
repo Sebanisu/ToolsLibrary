445baddcf4332a2f5072e72c382a87b5b20d7b21
Sebanisu <peeceseven@gmail.com>
2021-03-14 15:47:02 -0400

    added [[maybe_unused]]

--- src/tl/string.hpp
+++ src/tl/string.hpp
@@ -27,7 +27,7 @@ static void
  * @param input path string
  * @return modified string
  */
-static std::string
+[[maybe_unused]] static std::string
   remove_drive_letter(std::string &&input)
 {
   remove_drive_letter(input);
@@ -50,7 +50,7 @@ static void
  * Remove the \r from the end of the string
  * @param input
  */
-static std::string
+[[maybe_unused]] static std::string
   remove_carriage_return_from_end(std::string &&input)
 {
   remove_carriage_return_from_end(input);
@@ -69,7 +69,7 @@ static void
  * Remove the \r from the any part of the string
  * @param input
  */
-static std::string
+[[maybe_unused]] static std::string
   remove_carriage_return(std::string &&input)
 {
   remove_carriage_return(input);
@@ -103,11 +103,11 @@ static std::string
  * @param haystack string with slashes
  */
 [[maybe_unused]] static void
-undo_replace_slashes(std::string &haystack)
+  undo_replace_slashes(std::string &haystack)
 {
   if constexpr (std::filesystem::path::preferred_separator == '/') {
     std::ranges::replace(
-      haystack, std::filesystem::path::preferred_separator,'\\');
+      haystack, std::filesystem::path::preferred_separator, '\\');
   }
 }
 /**
@@ -115,7 +115,7 @@ undo_replace_slashes(std::string &haystack)
  * @param haystack string with slashes
  */
 [[maybe_unused]] static std::string
-undo_replace_slashes(std::string &&haystack)
+  undo_replace_slashes(std::string &&haystack)
 {
   undo_replace_slashes(haystack);
   return std::move(haystack);
