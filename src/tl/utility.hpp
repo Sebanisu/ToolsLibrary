5d5eeeb39b348c33a9c6c5df19c507ed269b52f4
Sebanisu <peeceseven@gmail.com>
2021-03-14 10:44:53 -0400

    added sequence to utility.

--- src/tl/utility.hpp
+++ src/tl/utility.hpp
@@ -1,17 +1,25 @@
 #ifndef TOOLSLIBRARY_UTILITY_HPP
 #define TOOLSLIBRARY_UTILITY_HPP
-#include <istream>
-#include <ranges>
-#include <span>
 #include <filesystem>
 #include <fstream>
+#include <istream>
 #include <optional>
+#include <ranges>
+#include <span>
 namespace tl::utility {
+/**
+ * Get remaining amount of bytes in a std::span.
+ * @param in span
+ */
 [[nodiscard]] static auto
   get_remaining(const std::span<const char> in)
 {
   return std::ranges::size(in);
 }
+/**
+ * Get remaining amount of bytes in a std::istream.
+ * @param in stream
+ */
 [[nodiscard]] static auto
   get_remaining(std::istream &in)
 {
@@ -21,8 +29,15 @@ namespace tl::utility {
   in.seekg(current, std::ios::beg);
   return static_cast<std::size_t>(end - current);
 }
+/**
+ * Create a temp file filled with data you want.
+ * @param file_name desired filename
+ * @param data data you want wrote to the file.
+ * @return optional path to file
+ */
 [[maybe_unused]] [[nodiscard]] static std::optional<std::filesystem::path>
-create_temp_file(const std::filesystem::path & file_name, const std::string_view & data)
+  create_temp_file(const std::filesystem::path &file_name,
+                   const std::string_view &     data)
 {
   using namespace std::string_view_literals;
   std::filesystem::path out_path =
@@ -36,5 +51,44 @@ create_temp_file(const std::filesystem::path & file_name, const std::string_view
   }
   return out_path;
 }
+/**
+ * Holds number sequence and has operator to pass a lambda that takes 1 of the
+ * sequence as a template parameter.
+ * @tparam number_sequence
+ */
+template<std::size_t... number_sequence> struct sequence_impl
+{
+  template<typename lambdaT>
+  constexpr void
+    operator()(const lambdaT &op) const
+  {
+    (op.template operator()<number_sequence>(), ...);
+  }
+};
+/**
+ *
+ * @tparam Min min value of I.
+ * @tparam I is the current number in sequence
+ * @tparam Ns sequence is number of values.
+ * @return
+ * @see https://stackoverflow.com/a/49673314/2588183
+ */
+template<std::size_t Min, std::size_t I, std::size_t... Ns>
+auto
+  make_sequence_impl()
+{
+  static_assert(I >= Min);
+  if constexpr (I == Min)
+    return sequence_impl<Ns...>();
+  else
+    return make_sequence_impl<Min, I - 1U, I - 1U, Ns...>();
+}
+/**
+ * Get sequence_impl from Min to Max numbers
+ * @tparam Min number
+ * @tparam Max number
+ */
+template<size_t Min, size_t Count>
+using sequence = std::decay_t<decltype(make_sequence_impl<Min, Count + Min>())>;
 }// namespace tl::utility
 #endif// TOOLSLIBRARY_UTILITY_HPP
---
+++ tests/src/sequence_test.cpp
@@ -0,0 +1,35 @@
+#include <boost/ut.hpp>// single header
+// import boost.ut;        // single module (C++20)
+#include "tl/utility.hpp"
+#include <string_view>
+int
+main()
+{
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+  using namespace boost::ut;
+  using namespace std::string_view_literals;
+  using namespace std::string_literals;
+  [[maybe_unused]] suite random = [] {
+    const auto check_type = []<std::integral integralT>() {
+      const auto random_values = tl::random::iota<char, 10U>();
+      expect(std::ranges::any_of(random_values, [](const auto &value) -> bool {
+        return value != integralT{};
+      }));
+    };
+    const auto check_types = [&check_type]<std::integral... integralT>()
+    {
+      (check_type.template operator()<integralT>(), ...);
+    };
+    "create random char array"_test = [&check_types] {
+      check_types.operator()<std::int8_t,
+        std::uint8_t,
+        std::int16_t,
+        std::uint16_t,
+        std::int32_t,
+        std::uint32_t,
+        std::int64_t,
+        std::uint64_t>();
+    };
+  };
+}
