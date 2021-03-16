d7cd1e182085145a5f12194c3123ece44cc784d5
Sebanisu <peeceseven@gmail.com>
2021-03-16 11:43:05 -0400

    suppress warning

--- tests/src/cmake/compiler_warnings.cmake
+++ tests/src/cmake/compiler_warnings.cmake
@@ -33,6 +33,7 @@ function(set_project_warnings project_name)
             /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
             /permissive- # standards conformance mode for MSVC compiler.
             #/wd4505 # suppress unreferenced local function has been removed.
+            /wd7576: suppress shadows a template parameter
             )
 
     set(CLANG_WARNINGS
