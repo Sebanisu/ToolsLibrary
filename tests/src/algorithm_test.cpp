b3b6688e7679adbc4ad5e66c91b34bc3f1cecee9
Sebanisu <peeceseven@gmail.com>
2021-03-17 19:26:16 -0400

    add test for for_each

--- tests/src/algorithm_test.cpp
+++ tests/src/algorithm_test.cpp
@@ -1,7 +1,28 @@
-//
-// Created by pcvii on 3/16/2021.
-//
+#include "boost/ut.hpp"// single header
+// import boost.ut;        // single module (C++20)
 #include "tl/algorithm.hpp"
+#include <string_view>
 int
   main()
-{}
+{
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+  using namespace boost::ut;
+  using namespace std::string_view_literals;
+  [[maybe_unused]] suite algorithm = [] {
+    static constexpr auto test_sv = "0123456789abcdefghijklmnopqrstuvwxyz"sv;
+    "for each test"_test          = [] {
+      tl::algorithm::for_each(
+        [](const auto &a) {
+          expect(neq(a, '\0'));
+        },
+        test_sv);
+      tl::algorithm::for_each(
+        [](const auto &a, const auto &b) {
+          expect(eq(a, b));
+        },
+        test_sv,
+        test_sv);
+    };
+  };
+}
