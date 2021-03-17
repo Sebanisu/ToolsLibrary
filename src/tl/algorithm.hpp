5c6db1220dedc326cc0178502999fd527df07fed
Sebanisu <peeceseven@gmail.com>
2021-03-17 17:53:09 -0400

    for_each ranges version.

--- src/tl/algorithm.hpp
+++ src/tl/algorithm.hpp
@@ -4,6 +4,7 @@
 
 #ifndef TOOLSLIBRARY_ALGORITHM_HPP
 #define TOOLSLIBRARY_ALGORITHM_HPP
+#include <ranges>
 namespace tl::algorithm {
 template<typename B1, typename E1, typename... BS, typename OP>
 void
@@ -12,6 +13,15 @@ void
   for (; b != e; ++b, (++bs, ...)) {
     op(*b, *bs...);
   }
+}
+template<std::ranges::forward_range R1, std::ranges::forward_range... RS, typename OP>
+void
+  for_each(const OP op, const R1 &r1, const RS &...rs)
+{
+  for_each(op,
+           std::ranges::cbegin(r1),
+           std::ranges::cend(r1),
+           std::ranges::cbegin(rs)...);
 }
 }// namespace tl::algorithm
 #endif// TOOLSLIBRARY_ALGORITHM_HPP
